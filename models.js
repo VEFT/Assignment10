daaf4cf12a32570252c26ecd0c4c63cc3d4042b1
danben4 <danielbenediktsson4@gmail.com>
2015-11-07 12:08:02 +0000

    flot

--- models.js
+++ models.js
@@ -3,22 +3,27 @@
 const mongoose = require('mongoose');
 
 const CompanySchema = mongoose.Schema({
-    name: {
-              type: String,
-      required: true,
-      minlength: 1
-          },
-      description: {
-                       type: String,
-      required: true,
-      minlength: 1
-                   },
-      punchcard_lifetime: {
-                              type: Number,
-      required: true
-                          }
+    title: {
+        type: String,
+        required: true,
+        minlength: 1
+    },
+    description: {
+        type: String,
+        required: true,
+        minlength: 1
+    },
+    url: {
+        type: String,
+        required: true,
+        minlength: 1
+    },
+    created: {
+        type: Date,
+        default: new Date()
+    }
 });
 
 module.exports = {
-    Company: mongoose.model('Company', CompanySchema),
+    Company: mongoose.model('Company', CompanySchema)
 };
